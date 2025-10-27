452819bbfbe3ec9e8752b205e1f1d42025b5d131
Anitha2418 <anitha.gaddigopula@hashagile.com>
2025-10-27 14:08:30 +0530

    Improved Code

--- src/App.java
+++ src/App.java
@@ -1,9 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to the java Application");
         for (int i=0;i<5;i++){
              System.out.println("This is my Application");
         }
         System.out.println("End");
         }
+
 }
