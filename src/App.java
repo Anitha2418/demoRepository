74da8720aba6edfbca7960b994a8d7aa98b5be49
Anitha2418 <anitha.gaddigopula@hashagile.com>
2025-10-27 13:59:58 +0530

    Updated message

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("This is my Application");
-        System.out.println("This is my Application");
-        
+        for (int i=0;i<5;i++){
+             System.out.println("This is my Application");
+        }
+        System.out.println("End");
         }
 }
