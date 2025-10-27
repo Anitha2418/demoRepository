458857387441deb18db628f3942db76a5a9aca09
Anitha2418 <anitha.gaddigopula@hashagile.com>
2025-10-27 15:53:51 +0530

    latest code

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to the java Application");
         for (int i=0;i<5;i++){
-             System.out.println("This is my Application");
+             System.out.println(" i value is :" +i);
         }
         System.out.println("End");
         }
